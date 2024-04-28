21f7b656ed7c4912e5bb1fd3b58b02efe418f377
Sameer Basil <smrbasil4@gmail.com>
2024-04-28 08:01:03 +0530

    Added config js file

---
+++ reading-list-front-end/public/config.js
@@ -0,0 +1,6 @@
+window.config = {
+  redirectUrl: "http://localhost:5173/login",
+  asgardeoClientId: "bmNzZllo2akhr4xO4W1oRfMf06Ya",
+  asgardeoBaseUrl: "https://api.asgardeo.io/t/sameer3079",
+  choreoApiUrl: "http://localhost:8080/reading-list",
+};
